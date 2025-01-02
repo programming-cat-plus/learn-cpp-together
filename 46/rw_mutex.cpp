d7ba8370d07a8d0c8965b47d70ee92031f379f9e
James <775592@qq.com>
2025-01-02 11:59:12 +0800

    add lesson 46

---
+++ 46/rw_mutex.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include "color.hpp"
+using namespace std::chrono_literals;
+
+struct SharedContent
+{
+    using high_resolution_clock = std::chrono::high_resolution_clock;
+    using float_seconds = std::chrono::duration<float, std::chrono::seconds::period>;
+
+public:
+    SharedContent() { startTime = high_resolution_clock::now(); }
+    void write()
+    {
+        std::srand(std::time(nullptr));
+        int random_number = std::rand();
+        std::stringstream stream;
+        stream << "rn=" << random_number << "\n";
+        std::lock_guard lock(mtx);
+        content = stream.str();
+        print(dye::yellow("write"));
+        std::this_thread::sleep_for(1s); // 模拟耗时操作
+    }
+    void read()
+    {
+        std::lock_guard lock(mtx);
+        print("read");
+        std::this_thread::sleep_for(500ms); // 模拟耗时操作
+    }
+
+private:
+    float elapsed_time() { return float_seconds(high_resolution_clock::now() - startTime).count(); }
+    template <class T>
+    void print(T &&rw)
+    {
+        std::lock_guard lock2(cout_mtx);
+        std::cout << std::fixed << std::setprecision(2) << std::setw(7) << elapsed_time() << "s:[tid:" << std::this_thread::get_id() << "][" << std::setw(5) << rw << "] " << content;
+    }
+
+private:
+    std::mutex mtx;
+    std::string content = "no data\n";
+    std::mutex cout_mtx;
+    high_resolution_clock::time_point startTime;
+};
+
+int main(void)
+{
+    SharedContent content;
+
+    auto read = [&content](){
+        for (int i = 0; i < 1000; i++){
+            content.read();
+            std::this_thread::sleep_for(1ms);
+        }
+    };
+
+    auto write = [&content](){
+        for (int i = 0; i < 1000; i++){
+            content.write();
+            std::this_thread::sleep_for(1ms);
+        }
+    };
+
+    std::thread w_th(write);
+    std::vector<std::thread> r_threads;
+    for (int i = 0; i < 4; i++)
+        r_threads.emplace_back(read);
+
+    w_th.join();
+    for (auto &t : r_threads)
+        t.join();
+}
